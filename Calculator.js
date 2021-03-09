e1434fefcaa47aff946385f114fb927c916beba0
Robin Meischke <robin@nierhain.de>
2021-03-09 21:18:48 +0100

    check if calculation is chainable

--- Calculator.js
+++ Calculator.js
@@ -1,5 +1,5 @@
 
-// input: '4+5/4 -6' | can contain: whitespaces, operators, numbers, false operators, false numbers
+// input: '4+5/4 -6' | can contain: whitespaces, operators, numbers, false operators, false numbers, parenthesis
 // output calculated value
 // get string -> trim string -> parse string -> calculate -> ??? -> profit
 
@@ -16,17 +16,20 @@ function calculate(calcString){
     numbers =  extractNumbers(calcString)
     operators = extractOperators(calcString)
 
-    do {
-        result += calculationStep(operators[0], numbers[0], numbers[1])
-        numbers.splice(0,2)
-        operators.shift()
-        numbers.unshift(result)
-    } while( numbers.length > 1)
-
+    if(isSingleOperation(operators) || isSameOperationType(operators)){
+        result = chainCalculation(numbers, operators[0])
+    } else {
+        result = arithmeticCalculation(numbers, operators)
+    }
     outputResult(result)
     return result
 }
 
+/*
+ *
+ *  PARSING INPUT
+ * 
+*/
 function trimString(str){
     return str.replace(/\s/g, '')
 }
@@ -43,6 +46,44 @@ function extractNumbers(str){
 
 function extractOperators(str){
     return str.replace(/\d/g, '').split('')
+}
+
+/*
+ *
+ *  CHECK OPERATION TYPE(S)
+ * 
+*/
+
+function isChainableCalculation(operators){
+    return isSameOperationType(operators) || isSingleOperation(operators)
+}
+
+function isSingleOperation(ops){
+    return ops.every((el) => {el == ADD_OPERATOR}) || ops.every((el) => {el == SUBTRACT_OPERATOR}) || ops.every((el) => {el == MULTIPLY_OPERATOR}) || ops.every((el) => {el == DIVIDE_OPERATOR})
+}
+
+function isSameOperationType(operators){
+    return operators.every(el => el == (ADD_OPERATOR || SUBTRACT_OPERATOR) || (MULTIPLY_OPERATOR || DIVIDE_OPERATOR))
+}
+
+/*
+ *
+ *  CALCULATIONS
+ * 
+*/
+
+function chainCalculation(numbers, operation){
+    let result = 0
+    do {
+        result += calculationStep(operation, numbers[0], numbers[1])
+        numbers.splice(0,2)
+        numbers.unshift(result)
+    } while( numbers.length > 1)
+    return result
+}
+
+function arithmeticCalculation(numbers, operators) {
+    
 }
 
 function calculationStep(operator, leftNumber, rightNumber) {
