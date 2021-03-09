46888265b9f3fadf0ae4e3bac81a53e293628228
Robin Meischke <robin@nierhain.de>
2021-03-09 23:09:51 +0100

    added arithmeticCalculation and user input

--- Calculator.js
+++ Calculator.js
@@ -3,7 +3,7 @@
 // output calculated value
 // get string -> trim string -> parse string -> calculate -> ??? -> profit
 
-let testCases = ['4+5 *3 -25/5','4+5/4-6','1+1']
+let testCases = ['4+5 *3 -25/5','4+5/4-6','1+1-1+2-3']
 
 const ADD_OPERATOR = '+'
 const SUBTRACT_OPERATOR = '-'
@@ -21,7 +21,6 @@ function calculate(calcString){
     } else {
         result = arithmeticCalculation(numbers, operators)
     }
-    outputResult(result)
     return result
 }
 
@@ -62,8 +61,8 @@ function isSingleOperation(ops){
     return ops.every((el) => {el == ADD_OPERATOR}) || ops.every((el) => {el == SUBTRACT_OPERATOR}) || ops.every((el) => {el == MULTIPLY_OPERATOR}) || ops.every((el) => {el == DIVIDE_OPERATOR})
 }
 
-function isSameOperationType(operators){
-    return operators.every(el => el == ADD_OPERATOR || el == SUBTRACT_OPERATOR ) || operators.every(el => el == MULTIPLY_OPERATOR || el == DIVIDE_OPERATOR)
+function isSameOperationType(ops){
+    return ops.every(el => el == ADD_OPERATOR || el == SUBTRACT_OPERATOR ) || ops.every(el => el == MULTIPLY_OPERATOR || el == DIVIDE_OPERATOR)
 }
 
 /*
@@ -84,7 +83,40 @@ function chainCalculation(numbers, operators){
 }
 
 function arithmeticCalculation(numbers, operators) {
+    let opIndex = 0, leftNumberIndex = 0, leftNumber = 0, result = 0
 
+    do {
+        // sample string: '4+5-6/12+7'
+        // sample values: [4,5,6,12,7]
+        // sample operators: ['+','-','/','+'] => 2 
+        // indices:: operator: 2 | leftNumber: op | rightNumber: op + 1
+
+        // find index of first multiplication/division
+        // find indices of "left" and "right" number
+        // calculate value
+        // delete indices from original arrays
+        // put result into array at leftNumberIndex
+
+        opIndex = operators.findIndex(checkOperation)
+        leftNumberIndex = opIndex
+        rightNumberIndex = leftNumberIndex + 1
+        leftNumber = calculationStep(operators[opIndex], numbers[leftNumberIndex], numbers[rightNumberIndex])
+        operators.splice(opIndex, 1)
+        numbers.splice(rightNumberIndex, 1)
+        numbers[leftNumberIndex] = leftNumber
+    } while(operators.includes(MULTIPLY_OPERATOR) || operators.includes(DIVIDE_OPERATOR))
+
+    if(numbers.length > 1){
+        result = chainCalculation(numbers, operators)
+    } else {
+        result = numbers[0] 
+    }
+
+    return result
+}
+
+function checkOperation(op){
+    return op == MULTIPLY_OPERATOR || op == DIVIDE_OPERATOR
 }
 
 function calculationStep(operator, leftNumber, rightNumber) {
@@ -106,17 +138,17 @@ function calculationStep(operator, leftNumber, rightNumber) {
     return result
 }
 
+function userInput(){
+    let userString = document.getElementById('inputfield').value
+    document.getElementById('inputResult').textContent = calculate(userString)
+}
 
 // DEBUGGING / OUTPUT
 function debugAll(){
-    testCases.forEach((testCase) => calculate(testCase))
+    testCases.forEach((testCase) => console.log(calculate(testCase)))
 }
 
 function debugSingle(index){
     res = calculate(testCases[index])
     document.getElementById('result').textContent = res
-}
-
-function outputResult(result){
-    console.log(result)
 }
